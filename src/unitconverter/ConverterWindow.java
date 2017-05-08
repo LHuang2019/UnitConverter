d1d5c6b9d23f5f5b11b04663cd03cb8abb513a43
Li Huang <lhuang210@gmail.com>
2017-05-07 21:22:32 -0400

    Implement event handler for converter combobox, which modifies unit combobox for source and target.

--- src/unitconverter/ConverterWindow.java
+++ src/unitconverter/ConverterWindow.java
@@ -2,13 +2,15 @@ package unitconverter;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.text.DecimalFormat;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class ConverterWindow extends JFrame {
 
-    private String[] converterList = { "Length" };
+    private String[] converterList = { "", "Length" };
     private Container window;
     private JLabel lblConverterType;
     private JLabel lblInput;
@@ -18,7 +20,7 @@ public class ConverterWindow extends JFrame {
     private JComboBox<String> cbxConverter;
     private JComboBox<String> cbxSourceUnit;
     private JComboBox<String> cbxTargetUnit;
-    
+
     private UnitConverter converter;
 
     public ConverterWindow()
@@ -26,9 +28,9 @@ public class ConverterWindow extends JFrame {
         window = getContentPane();
         window.setLayout(new FlowLayout());
         converter = new LengthConverter();
-        
+
         setup();
-        
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Unit Converter");
         setSize(300, 150);
@@ -42,41 +44,66 @@ public class ConverterWindow extends JFrame {
         setupSource();
         setupTarget();
     }
-    
+
     private void setupFrame()
     {
         lblConverterType = new JLabel("Conversion type: ", JLabel.LEFT);
         window.add(lblConverterType);
-        
+
         cbxConverter = new JComboBox<String>(converterList);
         window.add(cbxConverter);
-        
+
         lblInput = new JLabel("Input value: ", JLabel.LEFT);
         window.add(lblInput);
-        
+
         tfSource = new JFormattedTextField(new DecimalFormat());
         window.add(tfSource);
-        
+
         cbxSourceUnit = new JComboBox<String>();
         window.add(cbxSourceUnit);
-        
+
         lblOutput = new JLabel("Result value: ", JLabel.LEFT);
         window.add(lblOutput);
-        
+
         tfTarget = new JFormattedTextField(new DecimalFormat());
         window.add(tfTarget);
-        
+
         cbxTargetUnit = new JComboBox<String>();
         window.add(cbxTargetUnit);
     }
-    
+
     private void setupConverter()
     {
         cbxConverter.setSelectedIndex(0);
         cbxConverter.setPreferredSize(new Dimension(120, cbxConverter.getPreferredSize().height));
         ((JLabel)cbxConverter.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+        cbxConverter.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent event) {
+                if (event.getStateChange() == ItemEvent.SELECTED) {
+                    switch (cbxConverter.getSelectedItem().toString()) 
+                    {
+                    case "Length" :
+                        converter = new LengthConverter();
+                        cbxSourceUnit.removeAllItems();
+                        cbxTargetUnit.removeAllItems();
+                        for (int i = 0; i < converter.getUnit().length; i++)
+                        {
+                            cbxSourceUnit.addItem(converter.getUnit()[i]);
+                            cbxTargetUnit.addItem(converter.getUnit()[i]);
+                        }
+                        break;
+                    default :
+                        converter = null;
+                        cbxSourceUnit.removeAllItems();
+                        cbxTargetUnit.removeAllItems();
+                        break;
+                    }
+                }
+            } 
+        });
     }
-    
+
     private void setupSource()
     {
         tfSource.setColumns(10);
@@ -87,7 +114,7 @@ public class ConverterWindow extends JFrame {
     {
         tfTarget.setColumns(10);
     }
-    
+
     public static void main(String[] args)
     {
         new ConverterWindow();
