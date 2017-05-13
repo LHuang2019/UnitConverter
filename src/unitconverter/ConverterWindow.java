9411e2d6f1a5add52b67ddb1cb1ad69897193db6
LH <lhuang210@gmail.com>
2017-05-12 22:14:16 -0400

    Add event handler for selecting items in source and target unit combo box

--- src/unitconverter/ConverterWindow.java
+++ src/unitconverter/ConverterWindow.java
@@ -10,138 +10,153 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class ConverterWindow extends JFrame {
 
-    private String[] converterList = { "", "Length" };
-    
-    private JPanel panelConverter;
-    private JPanel panelSource;
-    private JPanel panelTarget;
-    
-    private JLabel lblConverterType;
-    private JLabel lblInput;
-    private JLabel lblOutput;
-    
-    private JFormattedTextField tfSource;
-    private JFormattedTextField tfTarget;
-    
-    private JComboBox<String> cbxConverter;
-    private JComboBox<String> cbxSourceUnit;
-    private JComboBox<String> cbxTargetUnit;
+	private String[] converterList = { "", "Length" };
 
-    private UnitConverter converter;
+	private JPanel panelConverter;
+	private JPanel panelSource;
+	private JPanel panelTarget;
 
-    public ConverterWindow()
-    {
-        setTitle("Unit Converter");
-        setSize(400, 200);
-        setLayout(new GridLayout(3, 1));
-        
-        setup();
-        
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
+	private JLabel lblConverterType;
+	private JLabel lblInput;
+	private JLabel lblOutput;
 
-    private void setup()
-    {
-        setupPanel();
-        setupComponent();
-        setupConverter();
-        setupSource();
-        setupTarget();
-    }
-    
-    private void setupPanel()
-    {
-        panelConverter = new JPanel();
-        panelConverter = new JPanel();
-        panelConverter.setLayout(new FlowLayout());
-        
-        panelSource = new JPanel();
-        panelSource = new JPanel();
-        panelSource.setLayout(new FlowLayout());
-        
-        panelTarget = new JPanel();
-        panelTarget = new JPanel();
-        panelTarget.setLayout(new FlowLayout());
-        
-        add(panelConverter);
-        add(panelSource);
-        add(panelTarget);
-    }
+	private JFormattedTextField tfSource;
+	private JFormattedTextField tfTarget;
 
-    private void setupComponent()
-    {
-        lblConverterType = new JLabel("Conversion type: ", JLabel.LEFT);
-        panelConverter.add(lblConverterType);
+	private JComboBox<String> cbxConverter;
+	private JComboBox<String> cbxSourceUnit;
+	private JComboBox<String> cbxTargetUnit;
 
-        cbxConverter = new JComboBox<String>(converterList);
-        panelConverter.add(cbxConverter);
+	private UnitConverter converter;
 
-        lblInput = new JLabel("Input value: ", JLabel.LEFT);
-        panelSource.add(lblInput);
+	public ConverterWindow()
+	{
+		setTitle("Unit Converter");
+		setSize(400, 200);
+		setLayout(new GridLayout(3, 1));
 
-        tfSource = new JFormattedTextField(new DecimalFormat());
-        panelSource.add(tfSource);
+		setup();
 
-        cbxSourceUnit = new JComboBox<String>();
-        panelSource.add(cbxSourceUnit);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 
-        lblOutput = new JLabel("Result value: ", JLabel.LEFT);
-        panelTarget.add(lblOutput);
+	private void setup()
+	{
+		setupPanel();
+		setupComponent();
+		setupConverter();
+		setupSource();
+		setupTarget();
+	}
 
-        tfTarget = new JFormattedTextField(new DecimalFormat());
-        panelTarget.add(tfTarget);
+	private void setupPanel()
+	{
+		panelConverter = new JPanel();
+		panelConverter = new JPanel();
+		panelConverter.setLayout(new FlowLayout());
 
-        cbxTargetUnit = new JComboBox<String>();
-        panelTarget.add(cbxTargetUnit);
-    }
+		panelSource = new JPanel();
+		panelSource = new JPanel();
+		panelSource.setLayout(new FlowLayout());
 
-    private void setupConverter()
-    {
-        cbxConverter.setSelectedIndex(0);
-        cbxConverter.setPreferredSize(new Dimension(120, cbxConverter.getPreferredSize().height));
-        ((JLabel)cbxConverter.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+		panelTarget = new JPanel();
+		panelTarget = new JPanel();
+		panelTarget.setLayout(new FlowLayout());
 
-        cbxConverter.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent event) {
-                if (event.getStateChange() == ItemEvent.SELECTED) {
-                    switch (cbxConverter.getSelectedItem().toString()) 
-                    {
-                    case "Length" :
-                        converter = new LengthConverter();
-                        cbxSourceUnit.removeAllItems();
-                        cbxTargetUnit.removeAllItems();
-                        for (int i = 0; i < converter.getUnit().length; i++)
-                        {
-                            cbxSourceUnit.addItem(converter.getUnit()[i]);
-                            cbxTargetUnit.addItem(converter.getUnit()[i]);
-                        }
-                        break;
-                    default :
-                        converter = null;
-                        cbxSourceUnit.removeAllItems();
-                        cbxTargetUnit.removeAllItems();
-                        break;
-                    }
-                }
-            } 
-        });
-    }
+		add(panelConverter);
+		add(panelSource);
+		add(panelTarget);
+	}
 
-    private void setupSource()
-    {
-        tfSource.setColumns(10);
+	private void setupComponent()
+	{
+		lblConverterType = new JLabel("Conversion type: ", JLabel.LEFT);
+		panelConverter.add(lblConverterType);
 
-    }
+		cbxConverter = new JComboBox<String>(converterList);
+		panelConverter.add(cbxConverter);
 
-    private void setupTarget()
-    {
-        tfTarget.setColumns(10);
-    }
+		lblInput = new JLabel("Input value: ", JLabel.LEFT);
+		panelSource.add(lblInput);
 
-    public static void main(String[] args)
-    {
-        new ConverterWindow();
-    }
+		tfSource = new JFormattedTextField(new DecimalFormat());
+		panelSource.add(tfSource);
+
+		cbxSourceUnit = new JComboBox<String>();
+		panelSource.add(cbxSourceUnit);
+
+		lblOutput = new JLabel("Result value: ", JLabel.LEFT);
+		panelTarget.add(lblOutput);
+
+		tfTarget = new JFormattedTextField(new DecimalFormat());
+		panelTarget.add(tfTarget);
+
+		cbxTargetUnit = new JComboBox<String>();
+		panelTarget.add(cbxTargetUnit);
+	}
+
+	private void setupConverter()
+	{
+		cbxConverter.setSelectedIndex(0);
+		cbxConverter.setPreferredSize(new Dimension(120, cbxConverter.getPreferredSize().height));
+		((JLabel)cbxConverter.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		cbxConverter.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent event) {
+				if (event.getStateChange() == ItemEvent.SELECTED) {
+					switch (cbxConverter.getSelectedItem().toString()) 
+					{
+					case "Length" :
+						converter = new LengthConverter();
+						cbxSourceUnit.removeAllItems();
+						cbxTargetUnit.removeAllItems();
+						for (int i = 0; i < converter.getUnit().length; i++)
+						{
+							cbxSourceUnit.addItem(converter.getUnit()[i]);
+							cbxTargetUnit.addItem(converter.getUnit()[i]);
+						}
+						break;
+					default :
+						converter = null;
+						cbxSourceUnit.removeAllItems();
+						cbxTargetUnit.removeAllItems();
+						break;
+					}
+				}
+			}
+		});
+	}
+
+	private void setupSource()
+	{
+		tfSource.setColumns(10);
+
+		cbxSourceUnit.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent event) {
+				if (event.getStateChange() == ItemEvent.SELECTED) {
+					converter.setSourceUnit(cbxSourceUnit.getSelectedItem().toString());
+				}
+			}
+		});
+	}
+
+	private void setupTarget()
+	{
+		tfTarget.setColumns(10);
+		
+		cbxTargetUnit.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent event) {
+				if (event.getStateChange() == ItemEvent.SELECTED) {
+					converter.setTargetUnit(cbxTargetUnit.getSelectedItem().toString());
+				}
+			}
+		});
+	}
+
+	public static void main(String[] args)
+	{
+		new ConverterWindow();
+	}
 
 }
